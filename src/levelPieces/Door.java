3364e9545dcae276b0e00c5662f1f7c7bb8d77ba
Gavin Garzia <ggarzia@mines.edu>
2024-01-30 15:31:09 -0700

    added door class

---
+++ src/levelPieces/Door.java
@@ -0,0 +1,15 @@
+package levelPieces;
+
+import gameEngine.Drawable;
+
+/*
+ * Class: Door
+ * Purpose: draws a door on the game board. Door does nothing.
+ */
+public class Door implements Drawable{
+	@Override
+	public void draw() {
+		System.out.println("D");
+	}
+	
+}
