13a19371bc4d2713ab1aaf8bd242a9241d963f9c
Gavin Garzia <ggarzia@mines.edu>
2024-01-31 18:03:48 -0700

    added toString and constructor to door class

--- src/levelPieces/Door.java
+++ src/levelPieces/Door.java
@@ -7,9 +7,23 @@ import gameEngine.Drawable;
  * Purpose: draws a door on the game board. Door does nothing.
  */
 public class Door implements Drawable{
+	private String label;
+	private char symbol;
+	
+	public Door() {
+		super();
+		this.label = "Door (its not locked, you can open it)";
+		this.symbol = 'D';
+	}
+
 	@Override
 	public void draw() {
-		System.out.println("D");
+		System.out.println(symbol);
+	}
+	
+	@Override
+	public String toString() {
+		return symbol + " - " + label ; 
 	}
 	
 }
