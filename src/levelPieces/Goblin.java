27569e678263737fab5df1229186300752667a34
Jacey Jonson <jjonson@mines.edu>
2024-02-03 20:42:03 -0700

    Move for Goblin

--- src/levelPieces/Goblin.java
+++ src/levelPieces/Goblin.java
@@ -2,12 +2,15 @@ package levelPieces;
 
 import gameEngine.Drawable;
 import gameEngine.InteractionResult;
+import gameEngine.Moveable;
 
-public class Goblin extends GamePiece {
+public class Goblin extends GamePiece implements Moveable {
 //Replicates Minion Class
+	int dir; 
 	
 	public Goblin(char symbol, String label, int location) {
 		super(symbol, label, location);
+		dir = -1; //starts the goblin moving left
 		// TODO Auto-generated constructor stub
 	}
 
@@ -15,6 +18,27 @@ public class Goblin extends GamePiece {
 	public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	@Override
+	public void move(Drawable[] gameBoard, int playerLocation) {
+		//moves left, jumping over pieces until at end of board and 
+		
+		//never jumps less than 2 spots at a time 
+		int curLocation = super.getLocation() + (1 * dir);
+		//try to move the piece a direction --> keep moving until open space
+		while(true) {
+			if(curLocation > gameBoard.length || curLocation < 0) {
+				dir *= -1;
+			} 
+			//move once 
+			curLocation += 1 * dir;
+			//check spot 
+			if(gameBoard[curLocation] == null) {
+				break;
+			}
+		}
+		super.setLocation(curLocation);
 	}
 	
 }
